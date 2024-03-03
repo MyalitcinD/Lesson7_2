78c2f28718647d146afac218f060c95ed23a8bb0
Денис <mden1996.07@gmail.com>
2024-03-03 04:19:49 +0300

    task 1+2 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -11,7 +11,7 @@ namespace Task1
         static double GetArea(double a, double b, double c)
         {
             double p = (a + b + c) / 2;
-            return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
         }
         static void Main(string[] args)
         {
@@ -19,7 +19,7 @@ namespace Task1
             double a1 = Convert.ToDouble(Console.ReadLine());
             double b1 = Convert.ToDouble(Console.ReadLine());
             double c1 = Convert.ToDouble(Console.ReadLine());
-            double z1 = GetArea(a1, b1,c1);
+            double z1 = GetArea(a1, b1, c1);
             Console.WriteLine("Введите стороны второго треугольника");
             double a2 = Convert.ToDouble(Console.ReadLine());
             double b2 = Convert.ToDouble(Console.ReadLine());
---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void GetCubeParams(double a, out double p, out double v)
+        {
+            p = a * a * 6;
+            v = a * a * a;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double v, p;
+            GetCubeParams(a, out p, out v);
+            Console.WriteLine("Объем куба = {0:f3}, площадь поверхности куба = {1:f3}", v, p);
+            Console.ReadKey();
+
+        }
+    }
+}
